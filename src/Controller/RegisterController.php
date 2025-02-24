e106e573101b2d32bef57848fa4411b0b9b8b178
Mike Agbelou <mike.agbelou@gmail.com>
2025-02-24 15:14:30 +0100

    update composer and symfony v-6.4

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
+        //si le formulaire est soumis alors:
+            //Tu enregistre les datas en BDD
+            //Tu envoies un message de confirmation du compte bien créé
+
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $em->persist($user);
+            $em->flush();
+        }
         return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView()
         ]);
