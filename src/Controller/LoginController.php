19be247423050579cf35ba50df31d492d59d974d
Mike Agbelou <mike.agbelou@gmail.com>
2025-03-12 18:43:21 +0100

    loginForm and userAuthUtils

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,11 +9,18 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/connexion', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        //Gérer les erreur d'auth
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        //Dernier username (email)
+        $lastUserName = $authenticationUtils->getLastUsername();
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'error' => $error,
+            'last_username' => $lastUserName,
         ]);
     }
 }
