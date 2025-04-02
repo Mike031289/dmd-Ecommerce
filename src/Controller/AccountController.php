1b2465174106bdba8b629a121e4024323d131c4a
Mike Agbelou <mike.agbelou@gmail.com>
2025-04-03 01:16:54 +0200

    password modification with eventLestener and error add

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AccountController extends AbstractController
@@ -15,8 +19,20 @@ final class AccountController extends AbstractController
     }
 
     #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
-    public function password(): Response
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
     {
-        return $this->render('account/password.html.twig');
+        $user = $this->getUser();
+        $form = $this->createForm(PasswordUserType::class, $user, [
+            'passwordHasher' => $passwordHasher
+        ]);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+        }
+        return $this->render('account/password.html.twig', [
+            'modifyPwd' => $form->createView()
+        ]);
     }
 }
