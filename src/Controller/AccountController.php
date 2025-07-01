77be87189145e95d51d40e7fbe5c71d62f4b2d39
Mike Agbelou <mike.agbelou@gmail.com>
2025-07-01 11:54:38 +0200

    cart implementation, session with Requestack

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -12,33 +12,33 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AccountController extends AbstractController
 {
-    #[Route('/compte', name: 'app_account')]
-    public function index(): Response
-    {
-        return $this->render('account/index.html.twig');
-    }
+   #[Route('/compte', name: 'app_account')]
+   public function index(): Response
+   {
+      return $this->render('account/index.html.twig');
+   }
 
-    #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
-    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
-    {
-        $user = $this->getUser();
-        $form = $this->createForm(PasswordUserType::class, $user, [
-            'passwordHasher' => $passwordHasher
-        ]);
+   #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
+   public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
+   {
+      $user = $this->getUser();
+      $form = $this->createForm(PasswordUserType::class, $user, [
+         'passwordHasher' => $passwordHasher
+      ]);
 
-        $form->handleRequest($request);
-        
-        if($form->isSubmitted() && $form->isValid()){
-            $em->flush();
-            
-            $this->addFlash(
-                'success',
-                'Votre mot de passe est correctement mis à jour'
-            );
-     
-        }
-        return $this->render('account/password.html.twig', [
-            'modifyPwd' => $form->createView()
-        ]);
-    }
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+         $em->flush();
+
+         $this->addFlash(
+            'success',
+            'Votre mot de passe est correctement mis à jour'
+         );
+
+      }
+      return $this->render('account/password.html.twig', [
+         'modifyPwd' => $form->createView()
+      ]);
+   }
 }
