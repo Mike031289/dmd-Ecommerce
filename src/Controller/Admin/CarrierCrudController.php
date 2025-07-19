f617c79ce791dab7fb8c8d30394fe6037ad9e14d
Mike Agbelou <mike.agbelou@gmail.com>
2025-07-19 16:25:31 +0200

    correct  new migration of Carrier entity to DB

--- src/Controller/Admin/CarrierCrudController.php
+++ src/Controller/Admin/CarrierCrudController.php
@@ -15,14 +15,14 @@ class CarrierCrudController extends AbstractCrudController
         return Carrier::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+    // /*
+    // public function configureFields(string $pageName): iterable
+    // {
+    //     return [
+    //         IdField::new('id'),
+    //         TextField::new('title'),
+    //         TextEditorField::new('description'),
+    //     ];
+    // }
+    // */
 }
